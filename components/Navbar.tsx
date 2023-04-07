c73016ed6411f20b0de1580c296b14c957f1f4bc
Amit Sharma <amit.sharma.mec21@itbhu.ac.in>
2023-04-07 19:47:30 +0530

    Added navbar , projects page

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,62 +1,43 @@
-import Link from 'next/link'
-import styles from '../styles/navbar.module.css'
-import Image from 'next/image';
-import Nav from "react-bootstrap/Nav";
 import Button from "react-bootstrap/Button";
+import Nav from "react-bootstrap/Nav";
+import Image from "next/image";
+import styles from "@/styles/navbar.module.css";
+import Link from "next/link";
 
+type NavBarProps = {
+  buttonstyle: string;
+  srclogo: string;
+  navbarColor: string;
+};
 
-function Navbar() {
+function NavBar({ srclogo, buttonstyle, navbarColor }: NavBarProps) {
   return (
-        <>
-      <div className={styles.navbar}>
+    <>
+      <div className={navbarColor}>
         <Link href="/">
-          <Image src="/images/SAEVectorWhite.svg" alt="SAE" width={150} height={50} />
+          <Image src={srclogo} alt="SAE" width={150} height={50} />
         </Link>
-        <Nav.Link href="/" className={styles.links}>
-          Home
-        </Nav.Link>
-        <Nav.Link href="/about" className={styles.links}>
-          About
-        </Nav.Link>
-        <Nav.Link href="/events" className={styles.links}>
-          Events
-        </Nav.Link>
-        <Nav.Link href="/blog" className={styles.links}>
-          Blogs
-        </Nav.Link>
-        <Nav.Link href="/projects" className={styles.links}>
-          Projects
-        </Nav.Link>
-        <Button className={styles.button}>Sign In</Button>
+        <div className={styles.linkContainer}>
+          <Nav.Link href="/" className={styles.links}>
+            Home
+          </Nav.Link>
+          <Nav.Link href="/about" className={styles.links}>
+            About
+          </Nav.Link>
+          <Nav.Link href="/events" className={styles.links}>
+            Events
+          </Nav.Link>
+          <Nav.Link href="/blog" className={styles.links}>
+            Blogs
+          </Nav.Link>
+          <Nav.Link href="/projects" className={styles.links}>
+            Projects
+          </Nav.Link>
+        </div>
+        <Button className={buttonstyle}>Sign In</Button>
       </div>
     </>
-    // <div className={styles.nav}>
-    //     <ul>
-    //         <li className={styles.nav_elements_left}>
-    //             <Link href="/" className={styles.logo}><Image src="/images/SAE.png" alt="SAE-logo" width={150} height={45}/></Link>
-    //         </li>
-    //         <li className={styles.nav_elements_mid}>
-    //             <Link href="/" className={styles.a}>Home</Link>
-    //         </li>
-    //         <li className={styles.nav_elements_mid}>
-    //             <Link href="/about" className={styles.a}>About Us</Link>
-    //         </li>
-    //         <li className={styles.nav_elements_mid}>    
-    //             <Link href="/events" className={styles.a}>Events</Link>
-    //         </li>
-    //         <li className={styles.nav_elements_mid}>
-    //             <Link href="/blog" className={styles.a}>Blog</Link>
-    //         </li>
-    //         <li className={styles.nav_elements_mid}>
-    //             <Link href="/projects" className={styles.a}>Projects and Achievements</Link>
-    //         </li>
-    //         <li className={styles.nav_elements_right}>
-    //             <Link href="/signin" className={styles.btn}><button id="signin">Sign In</button></Link>
-    //         </li>
-    //     </ul>
-    // </div>
-    
-  )
+  );
 }
 
-export default Navbar;
+export default NavBar;
--- pages/projects/index.tsx
+++ pages/projects/index.tsx
@@ -1,9 +1,23 @@
-import styles from '@/styles/projects.module.css'
+import styles from "@/styles/projects.module.css";
+import Navbar from "@/components/Navbar";
+import Image from "next/image";
+
+const srclogo = "/images/SAE.png";
+const buttonstyle = styles.button;
+const navbarColor = styles.navbar;
 
 export default function Projects() {
   return (
-    <div>
-      <h1 className={styles.h1}> Projects and Achievements</h1>
-    </div>
-  )
+    <>
+      <Navbar
+        srclogo={srclogo}
+        buttonstyle={buttonstyle}
+        navbarColor={navbarColor}
+      />
+      <div className={styles.achievements}>
+        <h1>Our Achievements</h1>
+        <h2>SAE(Society of Automotive Engineers) aims to give a platform to people who wish to learn about and build automobiles. Our teams take part in various RC and automotive events all round the year....</h2>
+      </div>
+    </>
+  );
 }
