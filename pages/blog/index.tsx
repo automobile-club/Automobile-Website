4b4b62ad687ddf45a0eef72ccaa076ead42a8ce3
SaiRev <saiyam.jain.mec21@itbhu.ac.in>
2023-04-17 14:34:17 +0530

    Blog Page FIxes

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -3,10 +3,6 @@ import styles from "@/styles/Blog.module.css";
 import Image from "next/image";
 import { useState } from "react";
 
-const srclogo = "/images/SAEVectorWhite.svg";
-const buttonstyle = styles.button;
-const navbarColor = styles.navbar;
-
 function BlogType() {
   const [selectedButton, setSelectedButton] = useState<string>("View All");
 
