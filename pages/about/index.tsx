d891f55e83b494e869b9284f9c6858a1732d040e
Rodent12 <jainsayyam1234@gmail.com>
2023-04-18 19:30:21 +0530

    Made About Page Responsive.

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -4,7 +4,7 @@ import ReactPlayer from 'react-player'
 export default function About() {
   return (
     <>
-    <Navbar BgColor="white"/>
+    <Navbar BgColor="black"/>
     <div className={styles.first_section}>
       <div className={styles.about}>
         <h1>About Us..</h1>
@@ -28,7 +28,7 @@ export default function About() {
               <img src="/images/TeamTrident.png" alt="team_trident"/>
             </div>
             <div className={styles.right}>
-            Team Trident is a group of IIT BHU engineering students preparing for Formula Bharat competition. They are designing a high-performance race car using advanced software tools and manufacturing techniques. The team is focusing on safety and reliability while rigorously testing their designs. They are confident in their skills and looking forward to competing against the best teams in the country.
+            Team Trident is a group of IIT BHU engineering students preparing for Formula Bharat competition. They are designing a high-performance race car using advanced software tools and manufacturing techniques. The team is focusing on safety and reliability while rigorously testing their designs.
             </div>
           </div>
     </div>
@@ -36,7 +36,7 @@ export default function About() {
           <h1>Team Rowbotics</h1>
           <div className={styles.content}>
           <div className={styles.right}>
-            Team Rowbotics is a group of talented engineering students from IIT BHU who are participating in an upcoming RC boat competition. Their goal is to design an autonomous boat that can efficiently navigate through water and complete tasks. The team is utilizing advanced computer-aided design software and cutting-edge manufacturing techniques to build their boat. They are focusing on developing a reliable, easy-to-control boat that is capable of accurate navigation.            
+            Team Rowbotics is a group of talented engineering students from IIT BHU who are participating in an upcoming RC boat competition. Their goal is to design an autonomous boat that can efficiently navigate through water and complete tasks. The team is utilizing advanced computer-aided design software and cutting-edge manufacturing techniques to build their boat.        
             </div>
             <div className={styles.left}>
               <img src="/images/TeamTrident.png" alt="team_trident"/>
@@ -50,7 +50,7 @@ export default function About() {
               <img src="/images/TeamTrident.png" alt="team_trident"/>
             </div>
             <div className={styles.right}>
-            Team RC Car, which consists of engineering students from IIT BHU, is getting ready for a track racing competition. Their objective is to develop a quick and effective car that can navigate the track with precision. The team is using advanced software tools for designing and up-to-date manufacturing techniques for building the car's components. The team is confident in their abilities and eagerly anticipating the challenge of competing against other skilled engineering teams from around the country.            
+            Team RC Car, which consists of engineering students from IIT BHU, is getting ready for a track racing competition. Their objective is to develop a quick and effective car that can navigate the track with precision. The team is using advanced software tools for designing and up-to-date manufacturing techniques for building the car's components. 
             </div>
           </div>
     </div>
