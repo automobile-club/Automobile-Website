49c2c48cff07370360e88c6659b4d20e57e4c381
ayush-beniwal <103768983+ayush-beniwal@users.noreply.github.com>
2023-04-18 21:03:46 +0530

    Footer work

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,45 +3,52 @@ import Link from "next/link";
 
 function Footer() {
   return (
-    <div className={styles.footer}>
-      <div className={styles.footer_box}>
-        <div className={styles.contact_us}>
-          <div className={styles.footer_logo}>
-            <img src="/images/bhuFooter.png" alt="" />
-            <img src="/images/logoFooter.png" alt="" />
-          </div>
-          <div className={styles.contact_us_desc}>
-            <h2>Contact Us</h2>
-            <span>
-              Indian Institute Of Technology
-              <br />
-              (BHU) Varanasi. India
-              <br />
-            </span>
-            <span>+91 7558525641</span>
-          </div>
+    <footer className={styles.footer_distributed}>
+      <div className={styles.footer_left}>
+        <h3>
+          SAE Collegiate Club <span>IIT (BHU) Varanasi</span>
+        </h3>
+
+        <p className={styles.footer_links}>
+          <a href="#" className={styles.link_1}>
+            Home
+          </a>
+
+          <a href="#">Events</a>
+
+          <a href="#">Blogs</a>
+
+          <a href="#">About</a>
+
+          <a href="#">Projects</a>
+        </p>
+
+        <p className={styles.footer_company_name}>+91 7558525641</p>
+      </div>
+
+      <div className={styles.footer_center}>
+        <img src='/images/Footerimg.png' alt="" />
+      </div>
+
+      <div className={styles.footer_right}>
+        <p className={styles.footer_company_about}>
+          <span>About SAE</span>
+          Lorem ipsum dolor sit amet, consectateur adispicing elit. Fusce
+          euismod convallis velit, eu auctor lacus vehicula sit amet.
+        </p>
+        <div>
+          <i className={"${styles.fa} ${styles.fa_phone}"}></i>
+          <p>+1.555.555.5555</p>
         </div>
-        <div className={styles.quick_links}>
-          <h2>Quick Links</h2>
-          <div className={styles.linkDiv}>
-            <Link href="/">Projects</Link>
-            <Link href="/">Events</Link>
-            <Link href="/">About us</Link>
-            <Link href="/">Workshops</Link>
-            <img src="/images/upArrow.svg" className={styles.upArrow} />
-          </div>
-          <h4 className={styles.connectLogo}>
-            Connect to us on :
-            <Link href="/">
-              <img src="/images/instagramFooter.png" alt="" />
-            </Link>
-            <Link href="/">
-              <img src="/images/linkedinFooter.png" alt="" />
-            </Link>
-          </h4>
+
+        <div>
+          <i className={"${styles.fa} ${styles.fa_envelope}"}></i>
+          <p>
+            <a href="mailto:support@company.com">support@company.com</a>
+          </p>
         </div>
       </div>
-    </div>
+    </footer>
   );
 }
 
