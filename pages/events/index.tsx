77d0125287d9f981cf088c03bc473af838a00581
Yogesh Sharma <116916363+Fusion509@users.noreply.github.com>
2023-10-26 02:15:58 +0530

    Update index.tsx

--- pages/events/index.tsx
+++ pages/events/index.tsx
@@ -2,20 +2,21 @@ import styles from "@/styles/events.module.css";
 import Image from "next/image";
 import Link from "next/link";
 import Navbar from "@/components/Navbar";
+import CustomCard from "./CustomCard";
 
 export default function Events() {
   return (
     <>
       <div>
-        <Navbar BgColor="white" />
+        <Navbar BgColor="black" />
         <div className={styles.upper_content}>
           <div className={styles.top_img_container}>
-            <Image
+            <Image 
+              className={styles.top_image}
               width={1200}
               height={800}
               src="/images/Rectangle37.png"
-              alt="#"
-              className={styles.top_image}
+              alt="#" 
             />
           </div>
           <div className={styles.btn_container}>
@@ -29,126 +30,11 @@ export default function Events() {
 
         <h1 className={styles.main_heading}>EVENT SCHEDULE</h1>
 
-        <div className={styles.all_events}>
-          <div className={styles.indivisual_event}>
-            <Link style={{ textDecoration: "none" }} href="/events/individual">
-              <Image
-                width={447}
-                height={290}
-                className={styles.event_img}
-                src="/images/Rectangle15.png"
-                alt="#"
-              ></Image>
-              <p className={styles.event_p}>February 6th 2023</p>
-              <h1 className={styles.event_h1}>Meet up Freelancer</h1>
-            </Link>
-          </div>
-          <div className={styles.indivisual_event}>
-            <Link style={{ textDecoration: "none" }} href="/events/individual">
-              <Image
-                width={447}
-                height={290}
-                className={styles.event_img}
-                src="/images/Rectangle15.png"
-                alt="#"
-              ></Image>
-              <p className={styles.event_p}>February 6th 2023</p>
-              <h1 className={styles.event_h1}>Meet up Freelancer</h1>
-            </Link>
-          </div>
-          <div className={styles.indivisual_event}>
-            <Link style={{ textDecoration: "none" }} href="/events/individual">
-              <Image
-                width={447}
-                height={290}
-                className={styles.event_img}
-                src="/images/Rectangle15.png"
-                alt="#"
-              ></Image>
-              <p className={styles.event_p}>February 6th 2023</p>
-              <h1 className={styles.event_h1}>Meet up Freelancer</h1>
-            </Link>
-          </div>
-          <div className={styles.indivisual_event}>
-            <Link style={{ textDecoration: "none" }} href="/events/individual">
-              <Image
-                width={447}
-                height={290}
-                className={styles.event_img}
-                src="/images/Rectangle15.png"
-                alt="#"
-              ></Image>
-              <p className={styles.event_p}>February 6th 2023</p>
-              <h1 className={styles.event_h1}>Meet up Freelancer</h1>
-            </Link>
-          </div>
-          <div className={styles.indivisual_event}>
-            <Link style={{ textDecoration: "none" }} href="/events/individual">
-              <Image
-                width={447}
-                height={290}
-                className={styles.event_img}
-                src="/images/Rectangle15.png"
-                alt="#"
-              ></Image>
-              <p className={styles.event_p}>February 6th 2023</p>
-              <h1 className={styles.event_h1}>Meet up Freelancer</h1>
-            </Link>
-          </div>
-          <div className={styles.indivisual_event}>
-            <Link style={{ textDecoration: "none" }} href="/events/individual">
-              <Image
-                width={447}
-                height={290}
-                className={styles.event_img}
-                src="/images/Rectangle15.png"
-                alt="#"
-              ></Image>
-              <p className={styles.event_p}>February 6th 2023</p>
-              <h1 className={styles.event_h1}>Meet up Freelancer</h1>
-            </Link>
-          </div>
-          <div className={styles.indivisual_event}>
-            <Link style={{ textDecoration: "none" }} href="/events/individual">
-              <Image
-                width={447}
-                height={290}
-                className={styles.event_img}
-                src="/images/Rectangle15.png"
-                alt="#"
-              ></Image>
-              <p className={styles.event_p}>February 6th 2023</p>
-              <h1 className={styles.event_h1}>Meet up Freelancer</h1>
-            </Link>
-          </div>
-          <div className={styles.indivisual_event}>
-            <Link style={{ textDecoration: "none" }} href="/events/individual">
-              <Image
-                width={447}
-                height={290}
-                className={styles.event_img}
-                src="/images/Rectangle15.png"
-                alt="#"
-              ></Image>
-              <p className={styles.event_p}>February 6th 2023</p>
-              <h1 className={styles.event_h1}>Meet up Freelancer</h1>
-            </Link>
-          </div>
-          <div className={styles.indivisual_event}>
-            <Link style={{ textDecoration: "none" }} href="/events/individual">
-              <Image
-                width={447}
-                height={290}
-                className={styles.event_img}
-                src="/images/Rectangle15.png"
-                alt="#"
-              ></Image>
-              <p className={styles.event_p}>February 6th 2023</p>
-              <h1 className={styles.event_h1}>Meet up Freelancer</h1>
-            </Link>
-          </div>
+        <div className="">
+        <CustomCard/>
+        <CustomCard/>
         </div>
-      </div>
+        </div>
     </>
   );
 }
