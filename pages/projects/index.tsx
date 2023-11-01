7e74376619f90d2d5995fd39ea93ee480c4193a8
sarpit2907 <sarpit2907@gmail.com>
2023-11-01 18:52:55 +0530

    Modification in projects page

--- pages/projects/index.tsx
+++ pages/projects/index.tsx
@@ -34,18 +34,20 @@ export default function Projects() {
       <Navbar BgColor="black" />
       <body className={styles.body}>
         <div className={`${styles.achievements} ${styles.introp}`}>
-          <h1>Our Achievements</h1>
+          <h1>Our Projects</h1>
           <span>
             <h2>
-              SAE(Society of Automotive Engineers) aims to give a platform to people who wish to learn about and build automobiles. Our teams take part in various RC and automotive events all round the year.... SAE(Society of Automotive Engineers) aims to give a platform to people who wish to learn about and build automobiles. Our teams take part in various RC and automotive events all round the year....
+            Discover our impressive portfolio of automotive projects that showcase our technical prowess and innovation.
+Each project represents our commitment to pushing the boundaries of automotive engineering.
+
             </h2>
           </span>
         </div>
         <div className={styles.timeline}>
         <div className={styles.Timeline_heading}>
-          <h2 className={styles.Timeline_heading_text}>Timeline</h2>
+          <h2 className={styles.Timeline_heading_text}> Check out some of our notable projects:</h2>
          </div>
-          <VerticalTimeline lineColor={"#8c96a3"}>
+          {/* <VerticalTimeline lineColor={"#8c96a3"}>
             <VerticalTimelineElement
               className="vertical-timeline-element--work"
               contentStyle={{ background: "#FF6012", color: "#f2f5f8" }}
@@ -109,10 +111,55 @@ export default function Projects() {
               </h4>
               <p>User Experience, Visual Design</p>
             </VerticalTimelineElement>
-          </VerticalTimeline>
+          </VerticalTimeline> */}
+          <div className={styles.card_container}>
+          <div className={styles.row}>
+            <div className={styles.card}>
+              <Image
+                src="/images/jlr _robotic_arm.png"
+                alt="Robotic arm"
+                width={290}
+                height={117.15}
+              />
+              <h2>JLR’s Robotic Arm Challenge</h2>
+              <p>
+              The JLR Automatic Robotic Charger Arm Challenge in Inter IIT 11.0 at IIT Kanpur was a remarkable platform for innovation, engineering excellence, and pushing the boundaries of automation. 
+              </p>
+              <a href="#">Read More</a>
+            </div>
+            <div className={styles.card}>
+              <Image
+                src="/images/piezoelectric_suspension.webp"
+                alt="Piezoelectric suspension"
+                width={290}
+                height={117.15}
+              />
+              <h2>Piezoelectric Suspension</h2>
+              <p>
+              The Piezoelectric Suspension Project undertaken by our team represents a pioneering leap into the realm of automotive engineering, offering a transformative solution to enhance vehicle dynamics, comfort, and sustainability.
+
+              </p>
+              <a href="#">Read More</a>
+            </div>
+            <div className={styles.card}>
+              <Image
+                src="/images/project-ev-logo.png"
+                alt="EV Project"
+                width={290}
+                height={117.15}
+              />
+              <h2>EV Project</h2>
+              <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec et ligula ut augue luctus venenatis. Aenean ipsum tortor, euismod sit amet sodales ac, commodo et dui. Maecenas auctor ex nibh, ut gravida orci iaculis ac. Proin facilisis eros nec laoreet scelerisque.
+              </p>
+              <a href="#">Read More</a>
+            </div>
+          </div>
+          </div>
         </div>
         <div className={styles.teams}>
-          <span>
+
+            <h1 >Our Vision</h1>
             <h2>
               SAE(Society of Automotive Engineers) aims to give a platform to
               people who wish to learn about and build automobiles. Our teams
@@ -125,7 +172,7 @@ export default function Projects() {
               Our teams take part in various RC and automotive events all round
               the year....
             </h2>
-          </span>
+    
         </div>
       </body>
     </>
