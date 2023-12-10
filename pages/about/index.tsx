36c242788686f6b6b1ee221e912614de6b10f866
Shashank Mittal <shashankmittal@Shashanks-Laptop.local>
2023-12-10 21:11:57 +0530

    fixes

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -5,18 +5,14 @@ export default function About() {
   return (
     <>
       <Navbar BgColor="black" />
-      <div className={`${styles.first_section} ${styles.introp}`}>
-        <h1>About Us...</h1>
-        <span>
-          <h2>
-            SAE(Society of Automotive Engineers) aims to give a platform to
-            people who wish to learn about and build automobiles. Our teams take
-            part in various RC and automotive events all round the year....
-            SAE(Society of Automotive Engineers) aims to give a platform to
-            people who wish to learn about and build automobiles. Our teams take
-            part in various RC and automotive events all round the year....
-          </h2>
-        </span>
+      <div className={styles.first_section}>
+        <div className={styles.about}>
+          <h1>About Us..</h1>
+          <div className={styles.desc}>
+            The SAE Collegiate Club at the Indian Institute of Technology (IIT) BHU is a dynamic and vibrant community of engineering enthusiasts dedicated to all things automotive. Our club serves as a melting pot of creativity, innovation, and engineering excellence, providing a platform for students to immerse themselves in the thrilling world of automobiles.
+          </div>
+          <hr />
+        </div>
         <div className={styles.star}>
           <img alt="stars" src="/images/star.png" />
         </div>
@@ -29,10 +25,10 @@ export default function About() {
             />
           </div>
           <div className={styles.intro_text}>Watch Our Intro Video<br></br>
-          <button className={styles.youtube_btn}>
-            Watch on &nbsp;
-            <img alt="youtube" src="/images/feYoutube2.png" />
-          </button>
+            <button className={styles.youtube_btn}>
+              Watch on &nbsp;
+              <img alt="youtube" src="/images/feYoutube2.png" />
+            </button>
           </div>
         </div>
       </div>
