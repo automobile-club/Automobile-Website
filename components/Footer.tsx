0abee4f4ec4c1734b804f5dce9e50894b75017ab
sarpit2907 <sarpit2907@gmail.com>
2024-01-15 01:32:43 +0530

    Changes in footer and project page

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,76 +1,172 @@
+import type { NextPage } from "next";
 import styles from "@/styles/Footer.module.css";
 import Link from "next/link";
+import { useState, useEffect, ChangeEvent, FormEvent } from "react";
+import ScrollToTopImage from '/Vector.png';
 
-function Footer() {
+
+const Group: NextPage = () => {
+  const [showScrollButton, setShowScrollButton] = useState(false);
+
+  useEffect(() => {
+
+    const handleScroll = () => {
+      const scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+      setShowScrollButton(scrollTop > 100);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  };
+  const [email, setEmail] = useState('');
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setEmail(e.target.value);
+  };
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setEmail((prevEmail) => {
+      console.log('Email submitted:', prevEmail);
+      return '';
+    });
+  };;
+
+
   return (
-    <div className={styles.footer}>
-      <div className={styles.footer_box}>
-        <div className={styles.footer_logo}>
-          <img src="/images/bhuFooter.svg" alt="" />
-          <img src="/images/logoFooter.svg" alt="" />
+    <div className={styles.rectangleParent}>
+      <div className={styles.groupChild} />
+      <div className={styles.groupItem} />
+      <div className={styles.instagramHeroParent}>
+        <div className={styles.instagramHero}>
+          <img className={styles.instagram_icon} alt="" src="/images/instagram_hero.svg" />
         </div>
-
-        <div className={styles.footer_line}>
-          <img src="/images/line 4.svg" alt="" />
+        <div className={styles.facebookHero}>
+          <img className={styles.facebook_icon} alt="" src="/images/facebook_hero.svg" />
         </div>
-
-        <div className={styles.contact_us_desc}>
-          <h2>Contact Us</h2>
-          <span>
-            Indian Institute Of Technology
-            <br />
-            (BHU) Varanasi, India
-            <br />
-          </span>
-          <span>+91 7558525641</span>
-          <br />
-          <br />
-          <span>Mail us at-</span>
-          <br />
-          <span className={styles.contact_us_desc_mail}>
-            <a href=" saecollegiateclub.iitbhu@gmail.com  ">
-              {" "}
-              <img src="/images/gmailFooter.svg" alt="" />
-            </a>
-            <span>
-              <Link href="saecollegiateclub.iitbhu@gmail.com">
-                saecollegiateclub.iitbhu@gmail.com
-              </Link>
-            </span>
-          </span>
+        <div className={styles.linkedinHero}>
+          <img className={styles.linkedin_icon} alt="" src="/images/Linkedin.svg" />
         </div>
-
-        <div className={styles.footer_line}>
-          <img src="/images/line 4.svg" alt="" />
+        <div className={styles.emailHero}>
+          <img className={styles.email_icon} alt="" src="/images/email@2x.png" />
         </div>
-
-        <div className={styles.quick_links}>
-          <h2>Quick Links</h2>
-          <div className={styles.linkDiv}>
-            <Link href="/projects">Projects</Link>
-            <Link href="/events">Events</Link>
-            <Link href="/about">About us</Link>
-            <Link href="/blog">Workshops</Link>
-            <img
-              alt="upArrow"
-              src="/images/upArrow.svg"
-              className={styles.upArrow}
-            />
+        <div className={styles.socials}>Socials</div>
+        <div className={styles.text}>
+          <div className={styles.instagram}>
+            <Link href="https://www.instagram.com/saecollegiateclubiitbhu/" style={{ color: 'var(--default-white)' }} >Instagram</Link>
           </div>
-          <h4 className={styles.connectLogo}>
-            Connect to us on :
-            <Link href="https://www.instagram.com/saecollegiateclubiitbhu/">
-              <img src="/images/instagramFooter.png" alt="" />
-            </Link>
-            <Link href="https://www.linkedin.com/company/sae-collegiate-club-iit-varanasi/">
-              <img src="/images/linkedinFooter.png" alt="" />
-            </Link>
-          </h4>
+          <div className={styles.facebook}>
+            <Link href="https://www.facebook.com/SAEIITBHU/" style={{ color: 'var(--default-white)' }} >Facebook</Link>
+          </div>
+          <div className={styles.linkediln}>
+            <Link href="https://www.linkedin.com/company/sae-collegiate-club-iit-varanasi/" style={{ color: 'var(--default-white)' }}> LinkedIn </Link>
+          </div>
+          <div className={styles.eMail}>
+            <Link href="mailto: saecollegiateclub.iitbhu@gmail.com" style={{ color: 'var(--default-white)' }}> Email</Link>
+          </div>
         </div>
       </div>
+      <div className={styles.groupParent}>
+        <div className={styles.collegiateClubParent}>
+          <div className={styles.collegiateClub}>            Collegiate Club</div>
+          <div className={styles.iitBhuVaranasiContainer}>
+            <span className={styles.iitBhuVaranasiContainer1}>
+              <p className={styles.iitBhuVaranasiIndia}>
+                IIT (BHU) Varanasi. India
+              </p>
+              <p className={styles.iitBhuVaranasiIndia}>PIN: 221005</p>
+            </span>
+          </div>
+          <div className={styles.tack}>
+            {`Dept. of Mechanical engineering`}</div>
+          <b className={styles.sae}>SAE</b>
+        </div>
+        <img className={styles.groupInner} alt="" src="/images/group-23@2x.png" />
+        <div className={styles.rectangleGroup}>
+          <div className={styles.rectangleDiv} />
+          <img className={styles.saeIcon} alt="" src="/images/sae@2x.png" />
+        </div>
+      </div>
+      <div className={styles.col3}>
+        <div className={styles.stayUpTo1}>Stay up to date</div>
+        <form className={styles.emailInputField1} onSubmit={handleSubmit}>
+          <div className={styles.inputFieldBg} >
+            <input
+              type="email"
+              value={email}
+              onChange={handleChange}
+              placeholder="Your email address"
+              className={styles.yourEmailAddress1}
+            />
+          </div>
+          <button type="submit" >
+            <img
+              src="/images/submit-icon.png"
+              alt="Submit"
+              className={styles.submit}
+            />
+          </button>
+        </form>
+      </div>
+      {showScrollButton && (
+        <img
+          className={styles.scrollButton}
+          src="/images/Vector.png"
+          alt="Scroll to Top"
+          onClick={scrollToTop}
+        />
+      )}
+      <div className={styles.nfoot}>
+        <div className={styles.nsocials}>
+          <div className={styles.ninstagramHero}>
+            <div className={styles.ninstagramHeroChild} />
+            <a href="https://www.instagram.com/saecollegiateclubiitbhu/" target="_blank">
+              <img className={styles.nvectorIcon} alt="" src="/images/instagram_hero.svg" />
+            </a>
+          </div>
+          <div className={styles.nfacebookHero}>
+            <div className={styles.nfacebookHeroChild} />
+            <a href="https://www.facebook.com/SAEIITBHU/" target="_blank">
+              <img className={styles.nvectorIcon1} alt="" src="/images/facebook_hero.svg" />
+            </a>
+          </div>
+          <div className={styles.nlinkedinHero}>
+            <div className={styles.nlinkedinChild} />
+            <a href="https://www.linkedin.com/company/sae-collegiate-club-iit-varanasi/" target="_blank">
+              <img className={styles.nlinkedinHeroIcon1} alt="" src="/images/linkedin_hero.svg" />
+            </a>
+          </div>
+          <div className={styles.nemailHero}>
+            <div className={styles.nemailChild} />
+            <a href="mailto: saecollegiateclub.iitbhu@gmail.com" target="_blank">
+              <img className={styles.nemailIcon} alt="" src="/images/email@2x.png" />
+              </a>
+          </div>
+      </div>
+      <div className={styles.nlogo}>
+        <img className={styles.nsaeIcon} alt="" src="/images/logo.svg" />
+      </div>
+      <div className={styles.nsaeCollegiateClub}>
+        <b className={styles.nsae}>SAE</b>
+        <div className={styles.ncollegiateClub}> Collegiate Club </div>
+      </div>
+      <div className={styles.niitBhuVaranasi}>IIT (BHU) Varanasi, India</div>
     </div>
+    </div >
+
+
   );
-}
+};
 
-export default Footer;
-
+export default Group;
--- pages/projects/index.tsx
+++ pages/projects/index.tsx
@@ -1,6 +1,7 @@
 import styles from "@/styles/projects.module.css";
 import React, { useState } from "react";
 import Navbar from "@/components/Navbar";
+import ProjectCard from "./projectcard";
 import {
   VerticalTimeline,
   VerticalTimelineElement,
@@ -15,6 +16,7 @@ import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 import "swiper/css/effect-coverflow";
+import Link from "next/link";
 
 export default function Projects() {
   const [slidesPerView, setSlidesPerView] = useState(3);
@@ -37,89 +39,203 @@ export default function Projects() {
           <h1>Our Projects</h1>
           <span>
             <h2>
-            Discover our impressive portfolio of automotive projects that showcase our technical prowess and innovation.
-Each project represents our commitment to pushing the boundaries of automotive engineering.
+              Discover our impressive portfolio of automotive projects that showcase our technical prowess and innovation.
+              Each project represents our commitment to pushing the boundaries of automotive engineering.
 
             </h2>
           </span>
         </div>
         <div className={styles.timeline}>
-        <div className={styles.Timeline_heading}>
-          <h1 className={styles.Timeline_heading_text}>Our notable projects:</h1>
-         </div>
-         </div>
-          <div className={styles.card_container}>
-  <div className={styles.card}>
-    <Image
-      src="/images/jlr robotic arm.png"
-      alt="Robotic arm"
-      width={290}
-      height={117.15}
-    />
-    <div className={styles.card_content}>
-      <h3>JLR’s Robotic Arm</h3>
-      <p>
-        The JLR Automatic Robotic Charger Arm Challenge in Inter IIT 11.0 at IIT
-        Kanpur was a remarkable platform for innovation, engineering excellence,
-        and pushing the boundaries of automation.
-      </p>
-      <a href="#">Read More</a>
-    </div>
-  </div>
-  <div className={styles.card}>
-    <Image
-      src="/images/piezoelectric_suspension.webp"
-      alt="Piezoelectric suspension"
-      width={290}
-      height={117.15}
-    />
-    <div className={styles.card_content}>
-      <h3>Piezoelectric Suspension</h3>
-      <p>
-        The Piezoelectric Suspension Project undertaken by our team represents a
-        pioneering leap into the realm of automotive engineering, offering a
-        transformative solution to enhance vehicle dynamics, comfort, and
-        sustainability.
-      </p>
-      <a href="#">Read More</a>
-    </div>
-  </div>
-  <div className={styles.card}>
-    <Image
-      src="/images/project-ev-logo.png"
-      alt="EV Project"
-      width={290}
-      height={117.5}
-    />
-    <div className={styles.card_content}>
-      <h3>EV Project</h3>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec et ligula
-        ut augue luctus venenatis. Aenean ipsum tortor, euismod sit amet sodales
-        ac, commodo et dui. Maecenas auctor ex nibh, ut gravida orci iaculis ac.
-        Proin facilisis eros nec laoreet scelerisque.
-      </p>
-      <a href="#">Read More</a>
-    </div>
-  </div>
-</div>
+          <div className={styles.Timeline_heading}>
+            <h1 className={styles.Timeline_heading_text}>Our notable projects:</h1>
+          </div>
+        </div>
+        <div className={styles.card_container}>
+          <div className={styles.card}>
+            <Image
+              src="/images/jlr robotic arm.png"
+              alt="Robotic arm"
+              width={290}
+              height={117.15}
+            />
+            <div className={styles.card_content}>
+              <h3>JLR’s Robotic Arm</h3>
+              <p>
+                The JLR Automatic Robotic Charger Arm Challenge in Inter IIT 11.0 at IIT
+                Kanpur was a remarkable platform for innovation, engineering excellence,
+                and pushing the boundaries of automation.
+              </p>
+              <a href="#">Read More</a>
+            </div>
+          </div>
+          <div className={styles.card}>
+            <Image
+              src="/images/piezoelectric_suspension.webp"
+              alt="Piezoelectric suspension"
+              width={290}
+              height={117.15}
+            />
+            <div className={styles.card_content}>
+              <h3>Piezoelectric Suspension</h3>
+              <p>
+                The Piezoelectric Suspension Project undertaken by our team represents a
+                pioneering leap into the realm of automotive engineering, offering a
+                transformative solution to enhance vehicle dynamics, comfort, and
+                sustainability.
+              </p>
+              <a href="#">Read More</a>
+            </div>
+          </div>
+          <div className={styles.card}>
+            <Image
+              src="/images/project-ev-logo.png"
+              alt="EV Project"
+              width={290}
+              height={117.5}
+            />
+            <div className={styles.card_content}>
+              <h3>EV Project</h3>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec et ligula
+                ut augue luctus venenatis. Aenean ipsum tortor, euismod sit amet sodales
+                ac, commodo et dui. Maecenas auctor ex nibh, ut gravida orci iaculis ac.
+                Proin facilisis eros nec laoreet scelerisque.
+              </p>
+              <a href="#">Read More</a>
+            </div>
+          </div>
+        </div>
 
-        <div className={styles.teams}>
+        <div className={styles.large}>
+          <div className={styles.third_section}>
+            <div className={styles.content}>
+              <div className={styles.right}>
+                <h2>Ongoing Projects</h2>
+                In the office, remote, or a mix of the two, with Miro, your team can connect, collaborate, and co-create in one space no matter where you are.
+                <Link href="#" className={`${styles.Btn}`}>
+                    Learn More
+                  </Link>
+              </div>
+              <div className={styles.ongoingprojects}>
+                <ProjectCard
+                  title="Formula Bharat"
+                  content="In the office, remote, or a mix of the two, with Miro, your team can connect, collaborate, and co-create in one space no matter where you are."
+                  imageSrc="/images/projects1.png"
+                />
+              </div>
+              <div className={`${styles.ongoingprojects} ${styles.middlecard}`}>
+                <ProjectCard
+                  title="Formula Bharat"
+                  content="In the office, remote, or a mix of the two, with Miro, your team can connect, collaborate, and co-create in one space no matter where you are."
+                  imageSrc="/images/projects2.png"
+                />
+              </div>
+              <div className={styles.ongoingprojects}>
+                <ProjectCard
+                  title="Formula Bharat"
+                  content="In the office, remote, or a mix of the two, with Miro, your team can connect, collaborate, and co-create in one space no matter where you are."
+                  imageSrc="/images/projects3.png"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
 
-            <h1 >Our Vision</h1>
-            <h2>
-              SAE(Society of Automotive Engineers) aims to give a platform to
-              people who wish to learn about and build automobiles. Our teams
-              take part in various RC and automotive events all round the
-              year.... SAE(Society of Automotive Engineers) aims to give a
-              platform to people who wish to learn about and build automobiles.
-              Our teams take part in various RC and automotive events all round
-              the year.... SAE(Society of Automotive Engineers) aims to give a
-              platform to people who wish to learn about and build automobiles.
-              Our teams take part in various RC and automotive events all round
-              the year....
-            </h2>
-    
+        <div className={styles.middle}>
+          <div className={styles.third_section}>
+            <div className={styles.content}>
+              <div className={styles.right}>
+                <h2>Ongoing Projects</h2>
+                In the office, remote, or a mix of the two, with Miro, your team can connect, collaborate, and co-create in one space no matter where you are.
+                  <Link href="#" className={`${styles.Btn}`}>
+                    Learn More
+                  </Link>
+              </div>
+              <div className={styles.lb}>
+                <div className={styles.ongoingprojects}>
+                  <ProjectCard
+                    title="Formula Bharat"
+                    content="In the office, remote, or a mix of the two, with Miro, your team can connect, collaborate, and co-create in one space no matter where you are."
+                    imageSrc="/images/projects1.png"
+                  />
+                </div>
+                <div className={`${styles.ongoingprojects} ${styles.middlecard}`}>
+                  <ProjectCard
+                    title="Formula Bharat"
+                    content="In the office, remote, or a mix of the two, with Miro, your team can connect, collaborate, and co-create in one space no matter where you are."
+                    imageSrc="/images/projects2.png"
+                  />
+                </div>
+                <div className={styles.ongoingprojects}>
+                  <ProjectCard
+                    title="Formula Bharat"
+                    content="In the office, remote, or a mix of the two, with Miro, your team can connect, collaborate, and co-create in one space no matter where you are."
+                    imageSrc="/images/projects3.png"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className={styles.small}>
+          <div className={styles.third_section}>
+            <div className={styles.content}>
+              <div className={styles.right}>
+                <h2>Ongoing Projects</h2>
+                In the office, remote, or a mix of the two, with Miro, your team can connect, collaborate, and co-create in one space no matter where you are.
+                <Link href="#" className={`${styles.Btn}`}>
+                    Learn More
+                  </Link>
+              </div>
+              <div className={styles.pcard_container}>
+                <div className={styles.pcard}>
+                  <Image
+                    src="/images/hybridwork.png (1).png"
+                    alt="Project 1"
+                    width={100}
+                    height={50}
+                  />
+                  <div className={styles.pcard_content}>
+
+                    <p>
+                      In the office, remote, or a mix of the two, with Miro, your team can connect, collaborate, and co-create in one space no matter where you are.
+                    </p>
+                  </div>
+                </div>
+                <div className={styles.pcard}>
+                  <Image
+                    src="/images/hybridwork.png (1).png"
+                    alt="Project 2"
+                    width={100}
+                    height={50}
+                  />
+                  <div className={styles.pcard_content}>
+
+                    <p>
+                      In the office, remote, or a mix of the two, with Miro, your team can connect, collaborate, and co-create in one space no matter where you are.
+                    </p>
+
+                  </div>
+                </div>
+                <div className={styles.pcard}>
+                  <Image
+                    src="/images/hybridwork.png (1).png"
+                    alt="Project 3"
+                    width={100}
+                    height={50}
+                  />
+                  <div className={styles.pcard_content}>
+
+                    <p>
+                      In the office, remote, or a mix of the two, with Miro, your team can connect, collaborate, and co-create in one space no matter where you are.
+                    </p>
+
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </body>
     </>
