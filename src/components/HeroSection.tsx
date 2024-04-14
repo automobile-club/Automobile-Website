563b3e922a8af385e0791c6978fe8fd05ed8730d
SaiRev <saiyam.jain.mec21@itbhu.ac.in>
2024-04-14 16:28:46 +0530

    Updated Images and Links to social media, Initiated Trident page

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -4,15 +4,26 @@ import Image from 'next/image'
 type HeroSectionProps = {
   title: string
   content: string
+  isTrident?: boolean
 }
 
-const HeroSection = ({ title, content }: HeroSectionProps) => {
+const HeroSection = ({
+  title,
+  content,
+  isTrident = false,
+}: HeroSectionProps) => {
   return (
     <div className={styles.sae_intro}>
       <h1 className={styles.sae_headings}>{title}</h1>
       <p className={styles.sae_long_text}>{content}</p>
       <div className={styles.social_media}>
-        <a href="https://www.instagram.com/saecollegiateclubiitbhu/">
+        <a
+          href={
+            !isTrident
+              ? 'https://www.instagram.com/saecollegiateclubiitbhu/'
+              : 'https://www.instagram.com/team_trident_iit_bhu/'
+          }
+        >
           <Image
             src="/images/social/instagram_hero.svg"
             alt="instagram"
@@ -20,7 +31,13 @@ const HeroSection = ({ title, content }: HeroSectionProps) => {
             height={54}
           />
         </a>
-        <a href="https://www.facebook.com/SAEIITBHU/">
+        <a
+          href={
+            !isTrident
+              ? 'https://www.facebook.com/SAEIITBHU/'
+              : 'https://www.facebook.com/TeamTridentIITBHU/'
+          }
+        >
           <Image
             src="/images/social/facebook_hero.svg"
             alt="facebook"
@@ -28,7 +45,13 @@ const HeroSection = ({ title, content }: HeroSectionProps) => {
             height={54}
           />
         </a>
-        <a href="https://in.linkedin.com/company/sae-collegiate-club-iit-varanasi">
+        <a
+          href={
+            !isTrident
+              ? 'https://in.linkedin.com/company/sae-collegiate-club-iit-varanasi'
+              : 'https://www.linkedin.com/company/team-trident-iit-bhu'
+          }
+        >
           <Image
             className={styles.linkedIn_img}
             src="/images/social/linkedin_hero.svg"
